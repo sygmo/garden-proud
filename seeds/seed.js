223293447144749dd488c7bf2bbfd9d2c837118e
Sydney Mercier <sydney.mercier91@gmail.com>
2022-02-15 19:45:09 -0600

    database seeds added to Garden table

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { User, Plant } = require('../models');
+const { User, Plant, Garden } = require('../models');
 
 const userData = require('./userData.json');
 const plantData = require('./plantData.json');
@@ -12,10 +12,24 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  console.log("User 2 id and email: " + users[1].id + " " + users[1].email);
+
   const plants = await Plant.bulkCreate(plantData, {
     individualHooks: true,
     returning: true,
   });
+
+  for (let i=0; i<10; i++) {
+    await Garden.create({
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      plant_id: plants[Math.floor(Math.random() * plants.length)].id
+    }, {
+      fields: ["user_id", "plant_id"]
+    })
+    .catch (err => {
+      console.log(err);
+    });
+  }
 
   process.exit(0);
 };
